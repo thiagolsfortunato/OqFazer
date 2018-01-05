57ef1be12e4dc954928afaa9415a1f9a144b9b03
Thiago Fortunato <thiago.fortunato@ftssjcd03.fotosensores.local>
2018-01-05 16:39:12 -0200

    Implatation City Domain

--- src/main/java/br/com/oqfazer/api/CityController.java
+++ src/main/java/br/com/oqfazer/api/CityController.java
@@ -1,4 +1,53 @@
 package br.com.oqfazer.api;
 
+import br.com.oqfazer.domain.city.City;
+import br.com.oqfazer.domain.city.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class CityController {
+
+    @Autowired
+    private CityService service;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(path = "/city", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<City> save(@RequestBody City city) {
+        City cityEntity = service.save(city);
+        return new ResponseEntity(cityEntity, HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(path = "/city", method = RequestMethod.PUT)
+    public ResponseEntity<City> edit(@RequestBody City city) {
+        City cityEntity = service.edit(city);
+        return new ResponseEntity<>(cityEntity, HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(path = "/city", method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@RequestBody City city) {
+        service.delete(city);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USERS')")
+    @RequestMapping(path = "/city", method = RequestMethod.GET)
+    public ResponseEntity<City> findByUsername(@RequestParam("name") String name) {
+        City cityEntity = service.findByName(name);
+        if (cityEntity != null) return new ResponseEntity<>(cityEntity, HttpStatus.OK);
+        else return new ResponseEntity<>(cityEntity, HttpStatus.NOT_FOUND);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(path = "/cities", method = RequestMethod.GET)
+    public ResponseEntity<City> getAll() {
+        return new ResponseEntity(service.getAllCities(), HttpStatus.OK);
+    }
 }
--- src/main/java/br/com/oqfazer/domain/city/CityService.java
+++ src/main/java/br/com/oqfazer/domain/city/CityService.java
@@ -1,4 +1,77 @@
 package br.com.oqfazer.domain.city;
 
+import br.com.oqfazer.domain.category.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CityService {
+
+    @Autowired
+    CityRepository repository;
+
+    public void setRepository(CityRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * O metodo e reponsavel por salvar e editar city
+     *
+     * @param city
+     * @return
+     */
+    public City save(final City city) {
+        return repository.save(city);
+    }
+
+    /**
+     * O metodo e reponsavel por editar city
+     *
+     * @param city
+     * @return
+     */
+    public City edit(final City city) {
+        return repository.save(city);
+    }
+
+    /**
+     * O metodo e reponsavel por deletar category
+     *
+     * @param category
+     */
+    public void delete(final City category) {
+        repository.delete(category);
+    }
+
+    /**
+     * O metodo e reponsavel por buscar todas cities
+     *
+     * @return List<City>
+     */
+    public List<City> getAllCities() {
+        return repository.findAll();
+    }
+
+    /**
+     * O metodo e reponsavel por buscar por nome da city
+     *
+     * @param name
+     * @return
+     */
+    public City findByName(final String name) {
+        return repository.findByName(name);
+    }
+
+    /**
+     * O metodo e reponsavel por buscar por id city
+     *
+     * @param id
+     * @return
+     */
+    public City findById(final Long id) {
+        return repository.findById(id);
+    }
+
 }
