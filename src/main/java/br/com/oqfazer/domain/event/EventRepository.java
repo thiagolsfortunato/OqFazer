3c4186c1bdc338ade5da0720ce69d152ab1bf379
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2018-01-08 17:57:04 -0200

    Implementation domain from Event

--- src/main/java/br/com/oqfazer/domain/event/EventRepository.java
+++ src/main/java/br/com/oqfazer/domain/event/EventRepository.java
@@ -1,4 +1,19 @@
 package br.com.oqfazer.domain.event;
 
-public interface EventRepository {
+import br.com.oqfazer.domain.region.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findByName(String name);
+
+    List<Event> findByRegion(String name);
+
+    List<Event> findByCategory(String name);
+
+    Event findById(Long id);
 }
